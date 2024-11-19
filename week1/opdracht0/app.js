60e1dc4dac92a69c490890c6a10bf0b75cfd080b
9027005 <9027005@student.zadkine.nl>
2024-11-19 13:28:28 +0100

    opdracht0

---
+++ week1/opdracht0/app.js
@@ -0,0 +1,10 @@
+let koek = document.getElementById("cookies")
+
+function cookies() {
+    //koek.style.display = "flex"
+    if (koek.style.display === "none") {
+        koek.style.display = "flex"
+    } else {
+        koek.style.display = "none"
+    }
+}
