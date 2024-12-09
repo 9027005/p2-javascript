ebf1d05f1fbe2ebcb6041b9da93b4e8a480ecaf4
9027005 <9027005@student.zadkine.nl>
2024-12-09 14:09:00 +0100

    opdracht 5

--- week2/opdracht5/app.js
+++ week2/opdracht5/app.js
@@ -1,4 +1,9 @@
-let list = document.getElementById("lijst")
-let listA = ["externe SSD","temp"]
 
-console.log(listA)
+let listA = ["externe SSD","amazon fire stick","Philips Performer Compact 3000 Series XD3110/09","kaas schaaf","XREAL Air 2 Pro AR Glasses"]
+
+
+//function lijst() {
+    for (i = 0;i <= 4; i++) {
+        let list = document.getElementById("lijst")
+        list.innerHTML += listA[i] + "<br>"
+    }
