439ca0703e3c4a8c1b2fe7eeb3763078d384b748
9027005 <9027005@student.zadkine.nl>
2024-12-17 13:00:09 +0100

    opdracht 7

---
+++ week4/opdracht7/app.js
@@ -0,0 +1,19 @@
+let block = document.getElementById("block")
+let width = window.innerWidth
+let  positie = 0
+let color = ["red","blue","yellow","green"]
+
+const timer = setInterval(function(){
+    positie += 20
+
+    if (positie > width) {
+        positie = 0
+    }
+    
+    block.style.left = positie + "px"
+},1000)
+
+block.addEventListener("click", function(){
+    let differentColor = Math.floor(Math.random() * 5)
+    block.style.backgroundColor = color[differentColor]
+})
